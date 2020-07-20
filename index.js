8d99f9f4735e05dce3405ff277511c9dd834ed2b
Sangwon <jackoss@naver.com>
2020-07-20 23:00:10 +0900

    오류 삭제

--- index.js
+++ index.js
@@ -38,39 +38,7 @@ app.post('/api/users/register', (req, res) => {
     })
 })
 
-app.post('/api/users/login', (req, res) => {
-    //요청된 이메일이 데이터베이스에 있는지 찾는다.
-    User.findOne({email: req.body.email}, (err, user) => {
-        if(!user){
-            return res.json({
-                loginSuccess: false,
-                message: "제공된 이메일에 해당하는 유저가 없습니다."
-            })
-        }
-        //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if(!isMatch){
-                return res.json({
-                    loginSuccess: false,
-                    message: "비밀번호가 틀렸습니다."
-                })
-            }
-            //비밀번호까지 같다면 Token을 생성
-            user.generateToken((err, user)=>{
-                if(err) return res.status(400).send(err);
 
-                // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 등등에 저장할 수 있다.
-                res.cookie("x_auth", user.token)
-                .status(200)
-                .json({
-                    loginSuccess: true,
-                    userId: user._id
-                })
-            })
-        })
-    })
-
-})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
