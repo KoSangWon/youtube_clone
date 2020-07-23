16386baa5956cf8fd0671cafe3f7bf488f9a0e7f
Sangwon <jackoss@naver.com>
2020-07-23 23:47:36 +0900

    copy

--- index.js
+++ index.js
@@ -73,30 +73,5 @@ app.post('/api/users/login', (req, res) => {
 })
 
 
-app.get("api/users/auth", auth, (req, res) => {
-    //여기까지 미들웨어가 통과했다는 것은 Authentication이 True라는 뜻
-    res.status(200).json({
-      _id: req.user._id,
-      //role 0 -> 일반유저, role 0 이 아니면 관리자
-      isAdmin: req.user.role === 0 ? false : true,
-      isAuth: true,
-      email: req.user.email,
-      name: req.user.name,
-      lastname: req.user.lastname,
-      role: req.user.role,
-      image: req.user.image
-    });
-  });
-  
-  app.get("/api/users/logout", auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-      if (err) return res.json({ success: false, err });
-      return res.status(200).send({
-        success: true
-      });
-    });
-  });
-  
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
